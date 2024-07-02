9f14a655029cc6623da9d11db447fdef6e4df9d6
Neha_08 <kankineha@gmail.com>
2024-07-02 22:05:58 +0530

    Time: 2 ms (97.13%), Space: 42.3 MB (99.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,32 @@
 class Solution {
+
+    // 3. Using freq array
+  
     public boolean isAnagram(String s, String t) {
-        char[] str1= s.toCharArray();
-        char[] str2=t.toCharArray();
-        if(str1.length!=str2.length){
-            return false;
-        }
-        Arrays.sort(str1);
-        Arrays.sort(str2);
-        
-        String s1= new String(str1);
-        String s2= new String(str2);
 
-        if(s1.equals(s2))
-        {
-            return true;
-        }
-         return false;
-      }
-
-      
-
+    int len1 = s.length();
+    int len2 = t.length();
+    if(len1!=len2){
+        return false;
     }
+     int[] freq = new int[26]; //to store count of the freq
+
+
+     for(int i =0;i<len1;i++){
+        char c= s.charAt(i);
+        freq[c -'a']++;
+     }
+
+     for(int i=0;i<len1;i++){
+        char c = t.charAt(i);
+        freq[c - 'a']--;
+     }
+
+     for(int count: freq){
+       if(count!=0){
+        return false;
+       }
+     }
+     return true;
+    }
+}
